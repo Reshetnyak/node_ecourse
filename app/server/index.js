137b9683c314a5286a2539c05e2d9559fabafb05
Denis Reshetniak <denis_reshetniak@epam.com>
2017-09-11 10:46:19 +0200

    Task-1: Add babel and build process

---
+++ app/server/index.js
@@ -0,0 +1,8 @@
+// import "babel-register";
+import { User, Product } from './models/index.js';
+
+// const User = require('./models/user');
+// const Product = require('./models/product');
+
+const Bob = new User();
+const Bread = new Product();
--- index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./models/user');
-const Product = require('./models/product');
-
-const Bob = new User();
-const Bread = new Product();
